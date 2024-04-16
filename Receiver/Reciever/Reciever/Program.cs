9710a838a04064a270d387ea57cd440ff7ad4fdd
yoavbakhar <147543992+yoavbakhar@users.noreply.github.com>
2024-04-16 17:38:07 +0300

    First attempt

--- Receiver/Reciever/Reciever/Program.cs
+++ Receiver/Reciever/Reciever/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Reciever
 {
     class Program
     {
+        private const int listenPort = 11000;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UdpClient udpClient = new UdpClient(listenPort);
+            try
+            {
+                // This will block until a message is received
+                Console.WriteLine("Waiting for UDP message...");
+                IPEndPoint remoteEP = null;
+                byte[] receivedData = udpClient.Receive(ref remoteEP);
+
+                string receivedMessage = Encoding.ASCII.GetString(receivedData);
+                Console.WriteLine($"Received message: {receivedMessage} from {remoteEP.Address}");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                udpClient.Close();
+            }
         }
     }
 }
