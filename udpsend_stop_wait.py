d51336914eeb1867233cb71dd15078990108414d
Leon <leon.andrassik@gmail.com>
2024-06-17 14:25:42 +0200

    add basic wait for ack to sender py

--- udpsend_stop_wait.py
+++ udpsend_stop_wait.py
@@ -8,8 +8,26 @@ class UDPTx:
     def __init__(self, data_len: int = 1024):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10485760)  # Increase send buffer size
+        self.sock.settimeout(10)  # Set a timeout for socket operations
         self.address = ('localhost', 4445)
         self.datalen = data_len
+
+    def wait_for_ack(self, seq_num: int):
+        while True:
+            try:
+                ack, _ = self.sock.recvfrom(8)
+                ack = struct.unpack('!ii', ack)
+                if ack[0] == 2:  # 2 is the ACK packet type
+                    break
+            except socket.timeout:
+                print(f"Timeout waiting for ACK for seq_num {seq_num}. Retrying...")
+                break
+                # Resend the last packet if needed or handle the timeout as appropriate
+                # For example, you can break or continue based on your retry logic
+                continue
+            except socket.error as e:
+                print(f"Socket error: {e}. Retrying...")
+                continue
 
     def send_file(self, file: str):
         with open(file, 'rb') as f:
@@ -31,6 +49,7 @@ class UDPTx:
             # Now start sending file data
             md5 = hashlib.md5()
             while True:
+                print("seqNumber: ", seq_number)
                 data = f.read(self.datalen)
                 if not data:
                     break
@@ -38,6 +57,9 @@ class UDPTx:
                 packet = struct.pack('!ii', transmission_id, seq_number) + data
                 self.sock.sendto(packet, self.address)
                 md5.update(data)
+                
+                self.wait_for_ack(seq_number)  # Break until ack packet is received
+
                 seq_number += 1
 
                 time.sleep(0.01)  # Small delay to avoid overwhelming the network
@@ -46,7 +68,6 @@ class UDPTx:
             md5_hash = md5.digest()
             final_packet = struct.pack('!ii', transmission_id, seq_number) + md5_hash
             self.sock.sendto(final_packet, self.address)
-
 
 def start():
     print("Which packet size? 1K/16K/64K")
@@ -66,4 +87,3 @@ def start():
     udp.sock.close()
 
 start()
-
