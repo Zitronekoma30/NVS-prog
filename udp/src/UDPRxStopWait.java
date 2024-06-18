d4e49016847ccecbf4785b73ab4c409b9b50090e
yoavbakhar <147543992+yoavbakhar@users.noreply.github.com>
2024-06-18 19:46:32 +0300

    Stop and Wait erledigt

---
+++ udp/src/UDPRxStopWait.java
@@ -0,0 +1,116 @@
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UDPReceiver {
+
+    private static final int BUFFER_SIZE = 64 * 1024; // 64KB
+
+    public static long receiveFile() throws IOException, NoSuchAlgorithmException {
+        DatagramSocket socket = new DatagramSocket(4444);
+        DatagramSocket ackSocket = new DatagramSocket();
+        socket.setReceiveBufferSize(1024 * 1024); // Set receive buffer to 1MB
+        InetAddress address = InetAddress.getByName("localhost");
+        int ackPort = 4447;
+
+        System.out.println("starting up on localhost port 4445");
+
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+
+        // Initialize variables
+        Integer transmissionId = null;
+        Integer maxSeqNumber = null;
+        String fileName = null;
+        ByteArrayOutputStream fileData = new ByteArrayOutputStream();
+        byte[] receivedMd5 = null;
+
+        System.out.println("listening on localhost:4444");
+
+        long startTime = 0;
+
+        while (true) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+
+            int packetLength = packet.getLength();
+            System.out.println("received " + packetLength + " bytes");
+
+            ByteBuffer wrapped = ByteBuffer.wrap(packet.getData(), 0, packetLength);
+
+            if (transmissionId == null) {
+                // This is the first packet, get transmissionId, maxSeqNumber, and file name
+                startTime = System.currentTimeMillis();
+                transmissionId = wrapped.getInt();
+                int seqNumber = wrapped.getInt();
+                maxSeqNumber = wrapped.getInt();
+                byte[] fileNameBytes = new byte[packetLength - 12];
+                wrapped.get(fileNameBytes);
+                fileName = new String(fileNameBytes).trim();
+                System.out.println("Receiving file: " + fileName);
+                System.out.println("seq_number: " + seqNumber);
+                System.out.println("max_seq_number: " + maxSeqNumber);
+            } else {
+                // Regular packet, get data
+                wrapped.position(4); // skip transmissionId
+                int seqNumber = wrapped.getInt();
+
+                if (seqNumber == maxSeqNumber) {
+                    System.out.println("final: " + seqNumber);
+                    // get md5
+                    receivedMd5 = new byte[packetLength - 8];
+                    wrapped.get(receivedMd5);
+                    break;
+                }
+
+                byte[] data = new byte[packetLength - 8];
+                wrapped.get(data);
+                fileData.write(data);
+                md5.update(data);
+                System.out.println("seq_number: " + seqNumber);
+            }
+
+            // Send ACK with seq_number
+            ByteBuffer ackBuffer = ByteBuffer.allocate(8);
+            ackBuffer.putInt(2);
+            ackBuffer.putInt(wrapped.getInt(4));
+            byte[] ack = ackBuffer.array();
+            DatagramPacket ackPacket = new DatagramPacket(ack, ack.length, address, ackPort);
+            ackSocket.send(ackPacket);
+        }
+
+        // Check MD5 hash
+        byte[] calculatedMd5 = md5.digest();
+
+        long receiveTime = System.currentTimeMillis() - startTime;
+        System.out.println("Time taken: " + receiveTime + " ms");
+
+        if (MessageDigest.isEqual(calculatedMd5, receivedMd5)) {
+            System.out.println("File received successfully");
+            try (FileOutputStream fos = new FileOutputStream(fileName)) {
+                fileData.writeTo(fos);
+            }
+        } else {
+            System.out.println("File was not received correctly");
+        }
+
+        socket.close();
+        return receiveTime;
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        List<Long> times = new ArrayList<>();
+        int filesSent = 0;
+
+        while (true) {
+            filesSent++;
+            long rcvTime = receiveFile();
+            times.add(rcvTime);
+            System.out.println("Average time: " + times.stream().mapToLong(Long::longValue).average().orElse(0.0) + " ms over " + filesSent + " files");
+        }
+    }
+}
---
+++ udp/src/UDPTxStopWait.java
@@ -0,0 +1,138 @@
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class UDPTx {
+    private DatagramSocket socket;
+    private InetAddress address;
+    private int port = 4444;
+    private int bindPort = 4447;
+    private int dataLen;
+
+    public UDPTx(int dataLen) throws SocketException, UnknownHostException {
+        this.dataLen = dataLen;
+        this.socket = new DatagramSocket(bindPort);
+        this.socket.setSoTimeout(10000); // Set timeout to 10 seconds
+        this.address = InetAddress.getByName("localhost");
+    }
+
+    private void waitForAck(int seqNum, byte[] packet) throws IOException {
+        while (true) {
+            try {
+                byte[] ackBuf = new byte[8];
+                DatagramPacket ackPacket = new DatagramPacket(ackBuf, ackBuf.length);
+                socket.receive(ackPacket);
+                ByteBuffer wrapped = ByteBuffer.wrap(ackBuf);
+                int packetType = wrapped.getInt();
+                int receivedSeqNum = wrapped.getInt();
+
+                if (packetType == 2) { // 2 is the ACK packet type
+                    break;
+                }
+            } catch (SocketTimeoutException e) {
+                System.out.println("Timeout waiting for ACK for seq_num " + seqNum + ". Retrying...");
+                DatagramPacket retryPacket = new DatagramPacket(packet, packet.length, address, port);
+                socket.send(retryPacket);
+            } catch (IOException e) {
+                System.out.println("Socket error: " + e.getMessage() + ". Retrying...");
+            }
+        }
+    }
+
+    public void sendFile(String filePath) throws IOException, NoSuchAlgorithmException {
+        File file = new File(filePath);
+        try (FileInputStream fis = new FileInputStream(file)) {
+            int transmissionId = 1;
+            int seqNumber = 0;
+            long fileSize = file.length();
+            String fileName = file.getName();
+            int maxSeqNumber = (int) Math.ceil((double) fileSize / dataLen) + 1;
+
+            System.out.println("maxSeqNumber: " + maxSeqNumber);
+            System.out.println("fileSize: " + fileSize);
+
+            // Send initial packet
+            ByteBuffer initialBuffer = ByteBuffer.allocate(12 + fileName.length());
+            initialBuffer.putInt(transmissionId);
+            initialBuffer.putInt(seqNumber);
+            initialBuffer.putInt(maxSeqNumber);
+            initialBuffer.put(fileName.getBytes());
+            byte[] initialPacket = initialBuffer.array();
+            DatagramPacket datagramPacket = new DatagramPacket(initialPacket, initialPacket.length, address, port);
+            socket.send(datagramPacket);
+
+            waitForAck(seqNumber, initialPacket);
+
+            seqNumber++;
+
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+
+            byte[] buffer = new byte[dataLen];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                ByteBuffer packetBuffer = ByteBuffer.allocate(8 + bytesRead);
+                packetBuffer.putInt(transmissionId);
+                packetBuffer.putInt(seqNumber);
+                packetBuffer.put(buffer, 0, bytesRead);
+                byte[] packet = packetBuffer.array();
+
+                datagramPacket = new DatagramPacket(packet, packet.length, address, port);
+                socket.send(datagramPacket);
+
+                md5.update(buffer, 0, bytesRead);
+
+                waitForAck(seqNumber, packet);
+
+                seqNumber++;
+
+                try {
+                    Thread.sleep(10); // Small delay to avoid overwhelming the network
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+            // Send the final packet with MD5 hash
+            byte[] md5Hash = md5.digest();
+            ByteBuffer finalBuffer = ByteBuffer.allocate(8 + md5Hash.length);
+            finalBuffer.putInt(transmissionId);
+            finalBuffer.putInt(seqNumber);
+            finalBuffer.put(md5Hash);
+            byte[] finalPacket = finalBuffer.array();
+            datagramPacket = new DatagramPacket(finalPacket, finalPacket.length, address, port);
+            socket.send(datagramPacket);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which packet size? 1K/16K/64K");
+        int size = (scanner.nextInt() * 1024) - 64;
+
+        System.out.println("Which file to send? 1MB/10MB/50MB/100MB");
+        int fileSize = scanner.nextInt();
+        String filePath = "./TestFiles/" + fileSize + "MB_file";
+
+        UDPTx udp = new UDPTx(size);
+
+        for (int i = 0; i < 1; i++) {
+            udp.sendFile(filePath);
+            System.out.println("Sent " + fileSize + "MB file " + (i + 1) + " times");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        udp.socket.close();
+        System.out.println("e to exit: ");
+        if (!scanner.next().equals("e")) {
+            main(args);
+        }
+    }
+}
